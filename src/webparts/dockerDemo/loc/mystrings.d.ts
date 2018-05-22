3dfc1ce243b3f0d95502540c5ec3a5d674f4f24c
ruudsolidnl <ruud@solidnl.nl>
2018-05-22 08:33:23 +0200

    Initial commit

---
+++ src/webparts/dockerDemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDockerDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DockerDemoWebPartStrings' {
+  const strings: IDockerDemoWebPartStrings;
+  export = strings;
+}
